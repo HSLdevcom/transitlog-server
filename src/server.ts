3f5f5df2a33400600df63e92c39adf6ad2eaccd0
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-02-22 07:53:56 +0200

    Add a redis cache for Apollo. Uses Redis through Docker.

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,16 @@ import schema from './schema'
 import { ApolloServer } from 'apollo-server'
 import { resolvers } from './resolvers/'
 import { createApp } from './app/app'
+import { RedisCache } from 'apollo-server-cache-redis'
 
 const app = createApp()
-const server = new ApolloServer({ typeDefs: schema, resolvers: resolvers(app) })
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers: resolvers(app),
+  cache: new RedisCache({
+    host: '0.0.0.0',
+  }),
+})
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`)
