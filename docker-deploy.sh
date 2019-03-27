e97c47b97b5051fa64c9a8f44aa61ab9eb4b12bf
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-03-27 08:53:46 +0200

    Add script for deploying to ocker hub locally.

---
+++ docker-deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# !!! You need
+
+set -e
+
+ORG=${ORG:-hsldevcom}
+DOCKER_TAG=latest
+DOCKER_IMAGE=$ORG/transitlog-server:${DOCKER_TAG}
+DOCKER_IMAGE_LATEST=$ORG/transitlog-server:latest
+
+docker build -t $DOCKER_IMAGE .
+
+docker push $DOCKER_IMAGE
+docker tag $DOCKER_IMAGE $DOCKER_IMAGE_LATEST
+docker push $DOCKER_IMAGE_LATEST
