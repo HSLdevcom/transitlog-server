d89f05fac845d9945919a154f03047658df1c14d
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-03-27 08:40:09 +0200

    Fix travis build script.

--- travis-build.sh
+++ travis-build.sh
@@ -3,8 +3,8 @@ set -e
 
 ORG=${ORG:-hsldevcom}
 DOCKER_TAG=${TRAVIS_BUILD_NUMBER:-latest}
-DOCKER_IMAGE=$ORG/hsl-map-reports:${DOCKER_TAG}
-DOCKER_IMAGE_LATEST=$ORG/hsl-map-reports:latest
+DOCKER_IMAGE=$ORG/transitlog-server:${DOCKER_TAG}
+DOCKER_IMAGE_LATEST=$ORG/transitlog-server:latest
 
 docker build -t $DOCKER_IMAGE .
 
