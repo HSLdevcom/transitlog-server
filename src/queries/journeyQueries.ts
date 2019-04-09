5cb0a5c4f6e5be0189fe69f25536f4720b472db0
Daniel Dunderfelt <daniel@developsuperpowers.com>
2019-04-09 10:58:11 +0300

    Increase max geohash level to 4 in area search query.

--- src/queries/journeyQueries.ts
+++ src/queries/journeyQueries.ts
@@ -86,7 +86,7 @@ export const AREA_EVENTS_QUERY = gql`
       order_by: { tsi: asc }
       where: {
         _and: [
-          { geohash_level: { _lte: 3 } }
+          { geohash_level: { _lte: 4 } }
           { oday: { _eq: $date } }
           { tst: { _lte: $maxTime } }
           { tst: { _gte: $minTime } }
