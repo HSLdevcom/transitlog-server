e1654d7bb609db468148da6de175fa7fa439753f
Daniel Dunderfelt <daniel@developsuperpowers.com>
2020-02-04 05:46:01 +0200

    Comment and fix mode fucntion.

--- src/utils/arrVal.ts
+++ src/utils/arrVal.ts
@@ -1,4 +1,10 @@
+import { compact } from 'lodash'
+
 // Wraps anything passed into an array. Falsy values result in an empty array.
-export const arrVal = <T = any>(itemOrItems: T | T[]): T[] => {
-  return !itemOrItems ? [] : Array.isArray(itemOrItems) ? itemOrItems : [itemOrItems]
+export const arrVal = <T = any>(
+  itemOrItems: T | T[],
+  filter: (val: T) => boolean = (val) => !!val
+): T[] => {
+  const arr = !itemOrItems ? [] : Array.isArray(itemOrItems) ? itemOrItems : [itemOrItems]
+  return compact(arr).filter(filter)
 }
--- src/utils/validModes.ts
+++ src/utils/validModes.ts
@@ -2,17 +2,37 @@ import { Mode } from '../types/Jore'
 import { arrVal } from './arrVal'
 import { compact, flatten, uniq } from 'lodash'
 
-export const validModes = (...modeArgs: any[]): Mode[] => {
+// The idea here is to throw in any number of whatevers and get out a list of unique
+// Mode strings. It converts single values to arrays and extracts modes from passed objects.
+
+type ObjWithMode = { mode?: string | Mode; modes?: string | Mode | Array<string | Mode> }
+type ModeArg = Array<string | Mode | ObjWithMode> | string | Mode | ObjWithMode
+
+function isObjWithMode(item): item is ObjWithMode {
+  return typeof item?.modes !== 'undefined' || typeof item?.modes !== 'undefined'
+}
+
+export const validModes = (...modeArgs: ModeArg[]): Mode[] => {
+  // Extract Mode values from each argument
   let allModes: any[] = modeArgs.map((modeArg: any) => {
     let val = modeArg
 
-    if (!Array.isArray(modeArg) && typeof modeArg !== 'string') {
+    // Check if we're dealing with an object with a .modes or .mode prop
+    if (isObjWithMode(modeArg)) {
       let possibleModes: any[] = []
 
+      // Slurp .modes if defined on the object
       if (typeof modeArg?.modes !== 'undefined') {
-        possibleModes = [...possibleModes, ...arrVal<Mode>(modeArg?.modes)]
+        possibleModes = [
+          ...possibleModes,
+          ...arrVal<Mode>(
+            modeArg?.modes as Mode[],
+            (modeItem) => typeof modeItem === 'string'
+          ),
+        ]
       }
 
+      // Pick .mode if defined on the object
       if (typeof modeArg?.mode !== 'undefined') {
         possibleModes.push(modeArg?.mode)
       }
@@ -20,15 +40,18 @@ export const validModes = (...modeArgs: any[]): Mode[] => {
       val = possibleModes
     }
 
-    return arrVal<Mode>(val)
+    // Ensure the val is an array.
+    return arrVal<Mode>(val, (modeItem) => typeof modeItem === 'string')
   })
 
   allModes = uniq(compact(flatten(allModes)))
 
+  // Convert mode to uppercase and METRO mode to SUBWAY
   const validModes: Mode[] = allModes.map((mode) => {
     const formattedMode = (!mode || typeof mode !== 'string' ? 'BUS' : mode).toUpperCase()
     return (formattedMode === 'METRO' ? 'SUBWAY' : formattedMode) as Mode
   })
 
+  // Always return an array, use BUS if empty.
   return validModes.length !== 0 ? validModes : [Mode.Bus]
 }
