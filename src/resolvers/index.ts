949341e32d0e03a212a644373ef1c9e06b1243e1
Joose Helle <joose.helle@gmail.com>
2020-04-06 11:47:28 +0300

    Refactor mutation resolvers for feedbacks.

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,4 +1,5 @@
 import { queryResolvers } from './queryResolvers'
+import { mutationResolvers } from './mutationResolvers'
 import { DateScalar } from './scalars/Date'
 import { TimeScalar } from './scalars/Time'
 import { DateTimeScalar } from './scalars/DateTime'
@@ -6,63 +7,10 @@ import { VehicleIdScalar } from './scalars/VehicleId'
 import { BBoxScalar, PreciseBBoxScalar } from './scalars/BBox'
 import { DirectionScalar } from './scalars/Direction'
 import { StringIndexed } from '../types/StringIndexed'
-import { WebClient } from '@slack/web-api'
-import { v4 as uuidv4 } from 'uuid'
-import { BlobServiceClient } from '@azure/storage-blob'
-
-const slack = new WebClient(process.env.SLACK_API_KEY)
-const azureStorageConn: any = process.env.AZURE_FEEDBACK_BLOB_CONN
-const azureBlobSas: any = process.env.AZURE_FEEDBACK_BLOB_SAS
 
 export const resolvers: StringIndexed<any> = {
   Query: { ...queryResolvers, uploads: (parent, args) => {} },
-  Mutation: {
-    sendFeedback: async (parent, args) => {
-      const { text, email, url } = args
-      console.log('received feedback args', args)
-      const feedbackRes: any = await slack.chat.postMessage({
-        channel: 'C010S7YF98E',
-        text: '*' + email.trim() + '*\n\n' + text.trim(),
-      })
-      const msgTs = feedbackRes.ts
-      await slack.chat.postMessage({
-        channel: 'C010S7YF98E',
-        thread_ts: msgTs,
-        text: '<' + url + '>',
-      })
-      return { text, email, msgTs }
-    },
-    uploadFeedbackImage: async (parent, { file, msgTs }) => {
-      const { createReadStream, filename, mimetype, encoding } = await file
-      console.log('uploading file', filename)
-
-      // upload image to azure blob
-      const blobName = uuidv4() + '.' + filename.split('.').pop()
-      const blobServiceClient = await BlobServiceClient.fromConnectionString(azureStorageConn)
-      const container = blobServiceClient.getContainerClient('feedback-images')
-      const blockBlob = container.getBlockBlobClient(blobName)
-      blockBlob.uploadStream(createReadStream())
-
-      // post download link to slack
-      const downloadLink =
-        '<https://feedbackfiles.blob.core.windows.net/feedback-images/' +
-        blobName +
-        azureBlobSas +
-        `|download attached image: ${filename}>`
-
-      console.log('downloadLink', downloadLink)
-      console.log('msgTs for upload', msgTs)
-
-      const imageLinkRes = await slack.chat.postMessage({
-        channel: 'C010S7YF98E',
-        thread_ts: msgTs,
-        text: downloadLink,
-      })
-
-      console.log('imageLinkRes', imageLinkRes)
-      return { filename, mimetype, encoding }
-    },
-  },
+  Mutation: mutationResolvers,
   Date: DateScalar,
   Time: TimeScalar,
   DateTime: DateTimeScalar,
